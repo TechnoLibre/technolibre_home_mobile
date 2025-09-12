9ecefc12e8fcbb981e20785ad367fb2111d269ee
Rafaël Huard <rafael.huard@technolibre.ca>
2025-09-12 15:00:30 -0400

    [ADD] NoteService dans l'env de Owl

--- technolibre_home/src/js/app.ts
+++ technolibre_home/src/js/app.ts
@@ -3,6 +3,7 @@ import { EventBus, mount } from "@odoo/owl";
 import { Constants } from "./constants";
 import { SimpleRouter } from "./router";
 import { AppService } from "./appService";
+import { NoteService } from "./noteService";
 
 const eventBus = new EventBus();
 
@@ -13,7 +14,8 @@ eventBus.addEventListener(Constants.ROUTER_NAVIGATION_EVENT_NAME, (event: any) =
 const router = new SimpleRouter();
 
 const appService = new AppService();
+const noteService = new NoteService();
 
-const env = { eventBus, router, appService };
+const env = { eventBus, router, appService, noteService };
 
 mount(RootComponent, document.body, { env });
