64893ee556499da6806515f10d7f74a3e678894e
Rafaël Huard <rafael.huard@technolibre.ca>
2025-09-12 11:29:08 -0400

    [ADD] Section principale de NotesComponent

--- technolibre_home/src/components/notes/notes_component.ts
+++ technolibre_home/src/components/notes/notes_component.ts
@@ -1,13 +1,18 @@
 import { Component, useState, xml } from "@odoo/owl";
 
+import { HeadingComponent } from "../heading/heading_component";
+
 export class NotesComponent extends Component {
 	static template = xml`
 		<div id="notes-component">
-			<h1>Notes</h1>
+			<HeadingComponent title="'Notes'"/>
+			<section id="notes">
+				<h2>Notes Section</h2>
+			</section>
 		</div>
 	`;
 
-	static components = {};
+	static components = { HeadingComponent };
 
 	state: any = undefined;
 
