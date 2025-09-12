8dc8d8a0639146da5926117b7ed542bd55b5fa3c
Rafaël Huard <rafael.huard@technolibre.ca>
2025-09-12 14:36:47 -0400

    [ADD] Base de NoteItemComponent

--- technolibre_home/src/components/notes/item/notes_item_component.ts
+++ technolibre_home/src/components/notes/item/notes_item_component.ts
@@ -1,10 +1,46 @@
 import { Component, useState, xml } from "@odoo/owl";
 
+import DeleteIcon from "../../../assets/icon/delete.svg";
+import EditIcon from "../../../assets/icon/edit.svg";
+
 export class NotesItemComponent extends Component {
 	static template = xml`
-		<div id="notes-item-component">
-			<h1>NotesItem</h1>
-		</div>
+		<li
+			class="notes-item"
+			t-on-click="() => this.props.openNote(props.note.id)"
+		>
+			<div class="notes-item__data">
+				<p class="notes-item__title">
+					<t t-esc="props.note.title"></t>
+				</p>
+				<p
+					class="notes-item__date"
+					t-att-class="{
+						'no-date': !props.note.date
+					}"
+				>
+
+					<t t-if="props.note.date" t-esc="props.note.date"></t>
+					<t t-else="" t-esc="'Sans date'"></t>
+				</p>
+			</div>
+			<div class="notes-item__actions">
+				<button
+					type="button"
+					class="notes-item__action notes-item__edit"
+					t-on-click.stop.prevent="() => this.props.editNote(props.note.id)"
+				>
+					<img src="${EditIcon}" />
+				</button>
+				<button
+					type="button"
+					class="notes-item__action notes-item__delete"
+					t-on-click.stop.prevent="() => this.props.deleteNote(props.note.id)"
+				>
+					<img src="${DeleteIcon}" />
+				</button>
+			</div>
+		</li>
 	`;
 
 	static components = {};
--- technolibre_home/src/components/notes/notes_component.ts
+++ technolibre_home/src/components/notes/notes_component.ts
@@ -1,22 +1,54 @@
 import { Component, useState, xml } from "@odoo/owl";
 
 import { HeadingComponent } from "../heading/heading_component";
+import { NotesItemComponent } from "./item/notes_item_component";
+import { Note } from "./types";
 
 export class NotesComponent extends Component {
 	static template = xml`
 		<div id="notes-component">
 			<HeadingComponent title="'Notes'"/>
 			<section id="notes">
-				<h2>Notes Section</h2>
+				<ul id="notes-list" t-if="state.notes.length !== 0">
+					<NotesItemComponent
+						t-foreach="state.notes"
+						t-as="noteItem"
+						t-key="noteItem.id"
+						note="noteItem"
+						openNote.bind="openNote"
+						editNote.bind="editNote"
+						deleteNote.bind="deleteNote"
+					/>
+				</ul>
+				<div id="notes-empty" t-else="">
+					<p>Il n'y a pas de note dans le stockage local.</p>
+				</div>
 			</section>
 		</div>
 	`;
 
-	static components = { HeadingComponent };
+	static components = { HeadingComponent, NotesItemComponent };
 
 	state: any = undefined;
 
 	setup() {
-		this.state = useState({});
+		this.state = useState({ notes: new Array<Note>() });
+
+		// Mock Notes
+		this.state.notes.push({ id: "1", title: "NoteOne" });
+		this.state.notes.push({ id: "2", title: "NoteTwo", date: "2025-09-12" });
+		this.state.notes.push({ id: "3", title: "NoteThree" });
+	}
+
+	openNote(noteId: string) {
+		console.log(`TODO: Open note ${noteId}.`);
+	}
+
+	editNote(noteId: string) {
+		console.log(`TODO: Edit note ${noteId}.`);
+	}
+
+	deleteNote(noteId: string) {
+		console.log(`TODO: Delete note ${noteId}.`);
 	}
 }
