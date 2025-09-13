6773d5091e13954a5451a07ed83f3eff89359ff6
Rafaël Huard <rafael.huard@technolibre.ca>
2025-09-13 15:57:37 -0400

    [ADD] Squelette de NoteComponent

--- technolibre_home/src/components/notes/note/note_component.ts
+++ technolibre_home/src/components/notes/note/note_component.ts
@@ -1,4 +1,9 @@
 import { Component, useState, xml } from "@odoo/owl";
+import { SimpleRouter } from "../../../js/router";
+import { NoteService } from "../../../js/noteService";
+import { Note } from "../types";
+import { Dialog } from "@capacitor/dialog";
+import { ErrorMessages } from "../../../js/errors";
 
 export class NoteComponent extends Component {
 	static template = xml`
@@ -12,6 +17,40 @@ export class NoteComponent extends Component {
 	state: any = undefined;
 
 	setup() {
-		this.state = useState({});
+		this.state = useState({
+			noteId: undefined,
+			note: undefined
+		});
+		this.setParams();
+		this.getNote();
+	}
+
+	private setParams() {
+		const router: SimpleRouter = this.env.router;
+		const params = router.getRouteParams(window.location.pathname);
+		this.state.noteId = decodeURIComponent(params["id"]);
+	}
+
+	private async getNote() {
+		const noteService: NoteService = this.env.noteService;
+		let matchingNote: Note | undefined;
+
+		try {
+			matchingNote = await noteService.getMatch(this.state.noteId);
+		} catch (error: unknown) {
+			if (error instanceof Error) {
+				Dialog.alert({ message: error.message });
+				return;
+			}
+		}
+
+		if (!matchingNote) {
+			Dialog.alert({ message: ErrorMessages.NO_NOTE_MATCH });
+			return;
+		}
+
+		console.log(this.state.note);
+		this.state.note = matchingNote;
+		console.log(this.state.note);
 	}
 }
