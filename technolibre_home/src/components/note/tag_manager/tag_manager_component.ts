78f4af7780125cfdd529369d5833100642328978
Rafaël Huard <rafael.huard@technolibre.ca>
2025-09-30 22:59:05 -0400

    [WIP] Interface de TagManagerComponent

--- technolibre_home/src/components/note/tag_manager/tag_manager_component.ts
+++ technolibre_home/src/components/note/tag_manager/tag_manager_component.ts
@@ -1,4 +1,4 @@
-import { onMounted, useRef, xml } from "@odoo/owl";
+import { onMounted, useRef, useState, xml } from "@odoo/owl";
 
 import { Constants } from "../../../js/constants";
 import { EnhancedComponent } from "../../../js/enhancedComponent";
@@ -9,11 +9,61 @@ export class TagManagerComponent extends EnhancedComponent {
 			id="tag-manager__popover"
 			popover=""
 			t-ref="tag-manager-popover"
-			t-on-click.stop.prevent="onTagManagerPopoverClick"
+			t-on-click.stop.prevent="hidePopover"
 		>
 			<div id="tag-manager__wrapper" t-on-click.stop.prevent="">
 				<div id="tag-manager">
-					Tag Manager
+					<section id="tag-manager__heading">
+						<h3>Tag Manager</h3>
+					</section>
+					<section id="tag-manager__top-controls">
+						<form
+							id="tag-manager__form"
+							t-on-input.stop.prevent="onSearchInput"
+						>
+							<input
+								id="tag-manager__search"
+								type="text"
+								placeholder="Rechercher"
+								t-model="state.search"
+							/>
+						</form>
+					</section>
+					<section id="tag-manager__content">
+						<ul
+							id="tag-manager__applied"
+							class="tag-manager__tag-list"
+						>
+							<li class="tag-manager__tag-list__item">Applied Tag</li>
+						</ul>
+						<ul
+							id="tag-manager__unapplied"
+							class="tag-manager__tag-list"
+						>
+							<li class="tag-manager__tag-list__item">Unapplied Tag</li>
+						</ul>
+					</section>
+					<section id="tag-manager__bottom-controls">
+						<a
+							id="tag-manager__create"
+							class="tag-manager__action"
+							href="#"
+							t-att-class="{
+								'disabled': isCreateDisabled()
+							}"
+							t-on-click.stop.prevent="onCreateTagClick"
+						>
+							Create
+						</a>
+						<a
+							id="tag-manager__close"
+							class="tag-manager__action"
+							href="#"
+							t-on-click.stop.prevent="hidePopover"
+						>
+							Close
+						</a>
+					</section>
 				</div>
 			</div>
 		</div>
@@ -22,6 +72,7 @@ export class TagManagerComponent extends EnhancedComponent {
 	tagManagerPopover = useRef("tag-manager-popover");
 
 	setup() {
+		this.state = useState({ search: "", tags: new Array<string>() });
 		onMounted(() => {
 			this.eventBus.addEventListener(Constants.TAG_MANAGER_EVENT_NAME, this.showPopover.bind(this));
 		});
@@ -32,14 +83,28 @@ export class TagManagerComponent extends EnhancedComponent {
 			return;
 		}
 
+		this.state.search = "";
 		this.tagManagerPopover.el.showPopover();
 	}
 
-	onTagManagerPopoverClick() {
+	hidePopover() {
 		if (!this.tagManagerPopover.el) {
 			return;
 		}
 
+		this.state.search = "";
 		this.tagManagerPopover.el.hidePopover();
+	}
+
+	onSearchInput() {
+		console.log(this.state.search);
+	}
+
+	onCreateTagClick() {
+		console.log("onCreateTagClick()");
+	}
+
+	isCreateDisabled(): boolean {
+		return this.state.search === "";
 	}
 }
