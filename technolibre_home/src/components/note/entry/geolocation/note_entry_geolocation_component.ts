cc673bb3d826d1172f3d628f1c3db26c5f14d36c
Rafaël Huard <rafael.huard@technolibre.ca>
2025-10-13 14:53:44 -0400

    [UPD] Déplacement du popover de géolocalisation

--- technolibre_home/src/components/note/entry/geolocation/note_entry_geolocation_component.ts
+++ technolibre_home/src/components/note/entry/geolocation/note_entry_geolocation_component.ts
@@ -23,29 +23,29 @@ export class NoteEntryGeolocationComponent extends EnhancedComponent {
 				</button>
 			</div>
 			<NoteEntryDragComponent editMode="props.editMode" />
-		</div>
-		<div
-			class="geolocation-popover"
-			popover=""
-			t-ref="geolocation-popover"
-			t-on-click.stop.prevent="hidePopover"
-		>
-			<div class="geolocation-display__wrapper" t-on-click.stop.prevent="">
-				<div class="geolocation-display">
-					<section class="geolocation-display__heading">
-						<h3>Données de géolocalisation</h3>
-					</section>
-					<section class="geolocation-display__content">
-						<p>
-							<b>Latitude:</b>&#160;<t t-esc="props.params.latitude"></t>
-						</p>
-						<p>
-							<b>Longitude:</b>&#160;<t t-esc="props.params.longitude"></t>
-						</p>
-						<p>
-							<b>Date:</b>&#160;<t t-esc="formatGeolocationTimestamp(props.params.timestamp)"></t>
-						</p>
-					</section>
+			<div
+				class="geolocation-popover"
+				popover=""
+				t-ref="geolocation-popover"
+				t-on-click.stop.prevent="hidePopover"
+			>
+				<div class="geolocation-display__wrapper" t-on-click.stop.prevent="">
+					<div class="geolocation-display">
+						<section class="geolocation-display__heading">
+							<h3>Données de géolocalisation</h3>
+						</section>
+						<section class="geolocation-display__content">
+							<p>
+								<b>Latitude:</b>&#160;<t t-esc="props.params.latitude"></t>
+							</p>
+							<p>
+								<b>Longitude:</b>&#160;<t t-esc="props.params.longitude"></t>
+							</p>
+							<p>
+								<b>Date:</b>&#160;<t t-esc="formatGeolocationTimestamp(props.params.timestamp)"></t>
+							</p>
+						</section>
+					</div>
 				</div>
 			</div>
 		</div>
