39740864dde12c0cf1553906b3cba91dd61af1c4
Rafaël Huard <rafael.huard@technolibre.ca>
2025-11-15 14:19:43 -0500

    [ADD] Support pour les send intents vidéo

--- technolibre_home/src/components/intent-handler/note/video/note_video_intent_handler_component.ts
+++ technolibre_home/src/components/intent-handler/note/video/note_video_intent_handler_component.ts
@@ -1,9 +1,39 @@
-import { Component, useState, xml } from "@odoo/owl";
+import { useState, xml } from "@odoo/owl";
+import { VideoIntent } from "../../../../models/intent";
+import { EnhancedComponent } from "../../../../js/enhancedComponent";
+import { Capacitor } from "@capacitor/core";
 
-export class NoteVideoIntentHandlerComponent extends Component {
+export class NoteVideoIntentHandlerComponent extends EnhancedComponent {
 	static template = xml`
 		<div id="note-video-intent-handler-component">
-			<h1>NoteVideoIntentHandler</h1>
+			<h1 class="intent__title">Ajouter une video</h1>
+			<video class="intent__thumbnail">
+				<source t-att-src="state.videoUri"></source>
+			</video>
+			<h3 class="intent__notes__title">Notes</h3>
+			<ul class="intent__notes" t-if="state.notes.length !== 0">
+				<li
+					class="intent__item intent__item--new"
+					t-on-click.stop.prevent="newNoteWithVideo"
+				>
+					Nouvelle note avec cette video
+				</li>
+				<li
+					class="intent__item"
+					t-foreach="state.notes"
+					t-as="note"
+					t-key="note.id"
+					t-att-data-id="note.id"
+					t-on-click.stop.prevent="event => this.addVideoToNote(event)"
+				>
+					<p class="intent__item__title" t-if="note.title">
+						<t t-esc="note.title"></t>
+					</p>
+					<p class="intent__item__title--empty" t-else="">
+						Sans titre
+					</p>
+				</li>
+			</ul>
 		</div>
 	`;
 
@@ -12,6 +42,45 @@ export class NoteVideoIntentHandlerComponent extends Component {
 	state: any = undefined;
 
 	setup() {
-		this.state = useState({});
+		this.state = useState({ videoUri: "", notes: [] });
+		this.getVideoUri();
+		this.getNotes();
+	}
+
+	newNoteWithVideo() {
+		const intent = this.props.intent;
+
+		if (!intent || !(intent instanceof VideoIntent)) {
+			return;
+		}
+
+		this.noteService.newNoteWithVideo(intent);
+		this.props.hidePopover();
+	}
+
+	addVideoToNote(event: Event) {
+		const id = (event.target as HTMLElement).dataset.id;
+		const intent = this.props.intent;
+
+		if (!id || !intent || !(intent instanceof VideoIntent)) {
+			return;
+		}
+
+		this.noteService.addVideoToNote(id, intent);
+		this.props.hidePopover();
+	}
+
+	public async getVideoUri() {
+		if (!this.props.intent?.url) {
+			return "";
+		}
+
+		const result = Capacitor.convertFileSrc(this.props.intent.url);
+
+		this.state.imageUri = result;
+	}
+
+	public async getNotes() {
+		this.state.notes = await this.noteService.getNotes();
 	}
 }
